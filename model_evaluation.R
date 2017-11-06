3034d519a69c8ae2fecdc2184142af4ee183c7ef
oadelso <oseas.adelso@gmail.com>
2017-11-06 01:05:47 -0800

    Update model_evaluation.R

--- model_evaluation.R
+++ model_evaluation.R
@@ -1,14 +1,16 @@
+#load required libraries
 library(ROCR)
 library(tidyverse)
 library(grid)
 library(gridExtra)
 
-#code start
+#testing data
 training_predictions=read.table("training_predictions.txt")
 colnames(training_predictions)=c('pred','label')
 training_predictions$boolean=ifelse(training_predictions$label=="Trump",1,0)
 pred=prediction(training_predictions$pred,training_predictions$boolean)
 perf=performance(pred,"tpr","fpr")
+#save first auc curve
 p0=plot(perf)
 auc=performance(pred,"auc")
 t_auc=unlist(slot(auc,"y.values"))
@@ -16,6 +18,7 @@ t_auc
 training_predictions$rounded=round(training_predictions$pred*2,1)/2
 hit_rate=training_predictions %>% group_by(rounded)%>%summarise(hit=mean(boolean),size=n(),probability=mean(pred))
 
+#calibration graph
 p1=ggplot(hit_rate,aes(x=probability,y=rounded))+
   geom_abline(intercept=0,slope=1,linetype="dotted")+
   labs(tite="Train Calibration", x="Hit Rate Model",y="Hit Rate Actual")
@@ -26,6 +29,8 @@ colnames(test_predictions)=c('pred','label')
 test_predictions$boolean=ifelse(test_predictions$label=="Trump",1,0)
 pred=prediction(test_predictions$pred,test_predictions$boolean)
 perf=performance(pred,"tpr","fpr")
+
+#save second auc curve
 p2=plot(perf)
 auc=performance(pred,"auc")
 t_auc=unlist(slot(auc,"y.values"))
@@ -34,6 +39,7 @@ t_auc
 test_predictions$rounded=round(test_predictions$pred*2,1)/2
 hit_rate_test=test_predictions %>% group_by(rounded)%>%summarise(hit=mean(boolean),size=n(),probability=mean(pred))
 
+#calibration graph
 p3=ggplot(hit_rate_test,aes(x=probability,y=rounded))+
   geom_point(aes(size=size))+
   geom_abline(intercept=0,slope=1,linetype="dotted")+
